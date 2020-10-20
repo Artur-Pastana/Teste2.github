f892870cd725c8ac50b68ab2bc7668914954224a
Artur-Pastana <reitutu_15@hotmail.com>
2020-10-20 12:31:22 -0300

    inserido variavel inteiro

--- secao12/Teste.java
+++ secao12/Teste.java
@@ -3,5 +3,7 @@ package secao12;
 public class Teste {
     public static void main(String[] args) {
         System.out.println("teste teste teste");
+        int n = 28;
+        System.out.println(n);
     }
 }
